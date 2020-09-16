7da12cad46500a0aba023cf2e9d51251c1638471
Daniel Rivas <danixrivas@gmail.com>
2020-09-16 14:13:45 +0200

    TEW-2021-408-2.1-Servlets con parámetros-

--- servletsv0_0/src/com/tew/Servlets/HolaMundoServlet.java
+++ servletsv0_0/src/com/tew/Servlets/HolaMundoServlet.java
@@ -29,12 +29,19 @@ public class HolaMundoServlet extends HttpServlet {
 	 */
     public void doGet (HttpServletRequest request, HttpServletResponse response)
     		 throws IOException, ServletException {
+    	
+    	String nombre = (String) request.getParameter("NombreUsuario");
+    	
     		 response.setCharacterEncoding("UTF-8");
     		 response.setContentType("text/html");
     		 PrintWriter out = response.getWriter();
     		 out.println("<HTML>");
     		 out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
     		 out.println("<BODY>");
+    		 
+    		 if(nombre != null) {
+    			 out.println("<br>Hola "+nombre+"<br>");
+    		 }
     		 out.println("Bienvenido a mi primera página web!");
     		 out.println("</BODY></HTML>");
     		}
