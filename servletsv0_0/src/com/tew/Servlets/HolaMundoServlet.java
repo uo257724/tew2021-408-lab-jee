bc0eb80493f061e08948e73be5d6ca1c8e119235
Daniel Rivas <danixrivas@gmail.com>
2020-09-16 13:54:55 +0200

    Carga inicial

---
+++ servletsv0_0/src/com/tew/Servlets/HolaMundoServlet.java
@@ -0,0 +1,50 @@
+package com.tew.Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HolaMundoServlet
+ */
+@WebServlet(name = "HolaMundo", urlPatterns = { "/HolaMundoCordial" })
+public class HolaMundoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HolaMundoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    public void doGet (HttpServletRequest request, HttpServletResponse response)
+    		 throws IOException, ServletException {
+    		 response.setCharacterEncoding("UTF-8");
+    		 response.setContentType("text/html");
+    		 PrintWriter out = response.getWriter();
+    		 out.println("<HTML>");
+    		 out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
+    		 out.println("<BODY>");
+    		 out.println("Bienvenido a mi primera página web!");
+    		 out.println("</BODY></HTML>");
+    		}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    		 throws IOException, ServletException {
+    		 doGet(request,response );
+    		}
+
+}
