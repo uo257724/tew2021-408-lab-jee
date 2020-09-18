1e3c3985deefea83d7fef0b6f40d83bcac9e2af9
Daniel <uo257724@uniovi.es>
2020-09-18 15:25:09 +0200

    TEW-2021-408-2.11-Ejercicios tienda de la compra con Servlets v2.

--- servletsv0_0/src/com/tew/Servlets/tienda/CarritoCompraServlet.java
+++ servletsv0_0/src/com/tew/Servlets/tienda/CarritoCompraServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -53,44 +54,48 @@ public class CarritoCompraServlet extends HttpServlet {
 
 		
 		
-		PrintWriter out = response.getWriter();
-		
-		out.println("<HTML>");
-		 out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
-		 out.println("<BODY>");
-		 
-		 out.println("<FORM>");
-		 
-		 out.println("<SELECT NAME=\"productos\" SIZE=\"1\">");
-		 
-			 out.println("<OPTION VALUE=\"010\">La trampa</OPTION>");
-			 out.println("<OPTION VALUE=\"345\">Los pajaros</OPTION>");
-			 out.println("<OPTION VALUE=\"554\">Matrix reloades</OPTION>");
-			 out.println("<OPTION VALUE=\"004\">Peli4</OPTION>");
-			 out.println("<OPTION VALUE=\"005\">Peli 5</OPTION>");
-			 out.println("<OPTION VALUE=\"006\">Peli 6</OPTION>");
-			 out.println("<OPTION VALUE=\"007\">Peli 7</OPTION>");
-			 out.println("<OPTION VALUE=\"008\">Peli 8</OPTION>");
-			 out.println("<OPTION VALUE=\"009\">Peli 9</OPTION>");
-			 out.println("<OPTION VALUE=\"011\">Peli 11</OPTION>");
-		 
-		 out.println("</SELECT>");
-		 out.println("<input type=\"submit\" value=\"Submit\">");
-		 out.println("</FORM>");
-		 
-		 for(Entry<String, Integer> entry : carrito.entrySet()) {
-			 
-			 String key = entry.getKey();
-			 Integer value = entry.getValue();
-			 out.println("<p>"+key+": "+value+"</p>");
-			 
-		 }
-		 
-		 out.println("</BODY>");
-		 out.println("</HTML>");	
+//		PrintWriter out = response.getWriter();
+//		
+//		out.println("<HTML>");
+//		 out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
+//		 out.println("<BODY>");
+//		 
+//		 out.println("<FORM>");
+//		 
+//		 out.println("<SELECT NAME=\"productos\" SIZE=\"1\">");
+//		 
+//			 out.println("<OPTION VALUE=\"010\">La trampa</OPTION>");
+//			 out.println("<OPTION VALUE=\"345\">Los pajaros</OPTION>");
+//			 out.println("<OPTION VALUE=\"554\">Matrix reloades</OPTION>");
+//			 out.println("<OPTION VALUE=\"004\">Peli4</OPTION>");
+//			 out.println("<OPTION VALUE=\"005\">Peli 5</OPTION>");
+//			 out.println("<OPTION VALUE=\"006\">Peli 6</OPTION>");
+//			 out.println("<OPTION VALUE=\"007\">Peli 7</OPTION>");
+//			 out.println("<OPTION VALUE=\"008\">Peli 8</OPTION>");
+//			 out.println("<OPTION VALUE=\"009\">Peli 9</OPTION>");
+//			 out.println("<OPTION VALUE=\"011\">Peli 11</OPTION>");
+//		 
+//		 out.println("</SELECT>");
+//		 out.println("<input type=\"submit\" value=\"Submit\">");
+//		 out.println("</FORM>");
+//		 
+//		 for(Entry<String, Integer> entry : carrito.entrySet()) {
+//			 
+//			 String key = entry.getKey();
+//			 Integer value = entry.getValue();
+//			 out.println("<p>"+key+": "+value+"</p>");
+//			 
+//		 }
+//		 
+//		 out.println("</BODY>");
+//		 out.println("</HTML>");	
 		 
 		//Añadimos el carrito a la sesión
 		request.getSession().setAttribute("carrito", carrito);
+		
+		 RequestDispatcher dispatcher =
+				 getServletContext().getNamedDispatcher("CarritoCompraVista");
+				dispatcher.forward(request, response);
 	}
 
 	/**
