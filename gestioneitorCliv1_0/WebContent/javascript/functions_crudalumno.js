21f6afe3c9dcb48ababdb671fe3d559f114ff5c8
Daniel <uo257724@uniovi.es>
2020-11-18 18:31:07 +0100

    TEW – 9.6 View – Método getIdAlumno

--- gestioneitorCliv1_0/WebContent/javascript/functions_crudalumno.js
+++ gestioneitorCliv1_0/WebContent/javascript/functions_crudalumno.js
@@ -91,23 +91,33 @@ this.list = function(lista) {
 }
 
 this.loadAlumnoFromForm = function() {
-	 // Cogemos el alumno nuevo del formulario.
-	 var alumno = {
-	 id : $("#id").val(),
-	 iduser : $("#iduser").val(),
-	 nombre : $("#nombre").val(),
-	 apellidos : $("#apellidos").val(),
-	 email : $("#email").val()
-	 };
-	 return alumno;
-	 }
+	// Cogemos el alumno nuevo del formulario.
+	var alumno = {
+			id : $("#id").val(),
+			iduser : $("#iduser").val(),
+			nombre : $("#nombre").val(),
+			apellidos : $("#apellidos").val(),
+			email : $("#email").val()
+	};
+	return alumno;
+}
 
 this.loadAlumnoInForm = function(alumno) {
-	 // Pintamos los datos alumnos sobre el formularios de alta/edición
-	 $("#id").val(alumno.id);
-	 $("#iduser").val(alumno.iduser);
-	 $("#nombre").val(alumno.nombre);
-	 $("#apellidos").val(alumno.apellidos);
-	 $("#email").val(alumno.email);
-	 $("#iduser").focus(); // Ponemos el foco en el campo Nombre.
-	 }
+	// Pintamos los datos alumnos sobre el formularios de alta/edición
+	$("#id").val(alumno.id);
+	$("#iduser").val(alumno.iduser);
+	$("#nombre").val(alumno.nombre);
+	$("#apellidos").val(alumno.apellidos);
+	$("#email").val(alumno.email);
+	$("#iduser").focus(); // Ponemos el foco en el campo Nombre.
+}
+
+This.getIdAlumno = function(celda) {
+	// Accedemos a la fila que está por encima de esta celda
+	// (closest('tr'))y despues obtenemos todas las celdas de esa fila
+	// (find('tr')) y
+	// nos quedamos con la segunda (get(1)) que es la contiene el "id" del
+	// alumno.
+	var id_alumno = parseInt(celda.closest('tr').find('td').get(1).innerHTML);
+	return id_alumno;
+} 
