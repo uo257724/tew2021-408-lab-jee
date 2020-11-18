4f3678784196a8715d97136e0d4dcff244643f21
Daniel <uo257724@uniovi.es>
2020-11-18 17:54:34 +0100

    TEW – 9.1 Model – Método Edit alumno

---
+++ gestioneitorCliv1_0/WebContent/javascript/functions_crudalumno.js
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+
+// Carga los datos del servicio sobreescribiendo el dato this.tbAlumnos.
+this.load = function() {
+	this.tbAlumnos = AlumnosServicesRs.getAlumnos();
+}
+
+
+// Llamamos al servicio de alta de alumno
+this.add = function(alumno) {
+	// Llamamos al servicio de alta de alumno
+	AlumnosServicesRs.saveAlumno({
+		$entity : alumno,
+		$contentType : "application/json"
+	});
+	// Recargamos la lista de alumnos.
+	this.load();
+}
+
+
+// Actualización de un alumno existente: PENDIENTE DE IMPLEMENTAR
+this.edit = function(alumno) {
+	
+	AlumnosServicesRs.updateAlumno({
+		$entity : alumno,
+		$contentType : "application/json"
+	});
+	
+	this.load();
+}
+
+
+//Eliminación un alumno existente
+this.remove = function(id_alumno) {
+	// Llamamos al servicio de borrado de alumno
+	AlumnosServicesRs.deleteAlumno({
+		id : id_alumno
+	});
+	// Recargamos la lista de alumnos.
+	this.load();
+}
+
+
+//Check alumno
+this.find = function(id_alumno) {
+	function checkAlumno(obj) {
+		return obj.id == id_alumno;
+	}
+	// Buscamos los datos del alumno cuyo id_alumno sea el mismo que el
+	// seleccionado
+	var alumno = this.tbAlumnos.find(checkAlumno);
+	return alumno;
+} 
